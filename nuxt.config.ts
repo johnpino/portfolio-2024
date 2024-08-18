dee0ea3f5a3cd0f2a813f66ad28c01ef6b1550f8
John Pino <iam@johnpino.me>
2024-08-18 18:52:40 -0500

    chore: update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  srcDir: 'src/'
 })
