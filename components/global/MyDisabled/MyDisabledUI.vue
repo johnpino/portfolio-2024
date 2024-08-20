8acc0022909224d0db0d8d50153d0f6be8983410
John Pino <iam@johnpino.me>
2024-08-19 21:40:15 -0500

    style: minor styling changes

--- components/global/MyDisabled/MyDisabledUI.vue
+++ components/global/MyDisabled/MyDisabledUI.vue
@@ -4,8 +4,8 @@
       <h1 class="text-4xl font-bold">{{ heading1stRow }} <br>
         <span class="text-6xl font-bold">{{ heading2ndRow }}</span>
       </h1>
-      <h2 class="text-base">{{ subheading }}</h2>
-      <p class="text-sm">{{ description }}</p>
+      <h2 class="text-base mb-3">{{ subheading }}</h2>
+      <p class="text-sm italic text-gray-600">{{ description }}</p>
     </div>
   </div>
 </template>
