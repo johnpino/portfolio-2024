06817650fdfef35c8a77c1f34016ae24b440b500
John Pino <iam@johnpino.me>
2024-09-03 17:37:07 -0500

    fix: fix issue between html content from openai and markdownit parser

--- composables/useOpenai.ts
+++ composables/useOpenai.ts
@@ -1,4 +1,4 @@
-import type OpenAI from "openai";
+import type OpenAI from 'openai'
 import markdownit from 'markdown-it'
 
 const resolveStream = async ({
@@ -7,50 +7,49 @@ const resolveStream = async ({
   onStart,
   stream,
 }: {
-  onChunk: (data: string) => void;
-  onReady: () => void;
-  onStart: () => void;
-  stream: ReadableStream<Uint8Array> | null;
+  onChunk: (data: string) => void
+  onReady: () => void
+  onStart: () => void
+  stream: ReadableStream<Uint8Array> | null
 }) => {
-
   if (!stream) return
 
-  const reader = stream.pipeThrough(new TextDecoderStream()).getReader();
+  const reader = stream.pipeThrough(new TextDecoderStream()).getReader()
 
   let onStartCalled = false
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    const stream = await reader.read();
-    if (stream.done) break;
+    const stream = await reader.read()
+    if (stream.done) break
 
     const chunks = stream?.value
-      .split("\n")
-      .filter((i) => Boolean(i.length))
-      .map((i) => JSON.parse(i)) as Array<OpenAI.Beta.AssistantStreamEvent>;
+      .split('\n')
+      .filter(i => Boolean(i.length))
+      .map(i => JSON.parse(i)) as Array<OpenAI.Beta.AssistantStreamEvent>
 
     for (const chunk of chunks) {
-      if (chunk.event !== 'thread.message.delta' ||
-        !chunk.data.delta.content?.length ||
-        chunk.data.delta.content[0].type !== 'text' ||
-        !chunk.data.delta.content[0].text?.value) continue
+      if (chunk.event !== 'thread.message.delta'
+        || !chunk.data.delta.content?.length
+        || chunk.data.delta.content[0].type !== 'text'
+        || !chunk.data.delta.content[0].text?.value) continue
 
       if (!onStartCalled) {
         onStart()
         onStartCalled = true
       }
 
-      onChunk(chunk.data.delta.content[0].text.value);
+      onChunk(chunk.data.delta.content[0].text.value)
     }
   }
 
-  onReady();
-};
+  onReady()
+}
 
 const createThread = async (ref: Ref, initialMessage?: string) => {
   const data = await $fetch('api/create/thread', {
     method: 'post',
-    body: { initialMessage }
+    body: { initialMessage },
   })
   ref.value = data
 }
@@ -60,11 +59,11 @@ interface UseOpenaiProps {
 }
 
 export const useOpenai = (props: UseOpenaiProps = {}) => {
-  const md = markdownit()
+  const md = markdownit({ html: true })
   const isLoading = ref(false)
   const threadId = ref()
 
-  watch(threadId, val => {
+  watch(threadId, (val) => {
     localStorage.setItem('threadId', val)
   })
 
@@ -82,16 +81,15 @@ export const useOpenai = (props: UseOpenaiProps = {}) => {
 
   const answer = reactive({
     role: 'assistant',
-    content: ''
+    content: '',
   })
 
   const sendMessage = async (message: string) => {
-
     isLoading.value = true
 
     messages.push({
       role: 'user',
-      content: message
+      content: message,
     })
 
     const stream = await fetch('api/create/message',
@@ -99,7 +97,7 @@ export const useOpenai = (props: UseOpenaiProps = {}) => {
         method: 'post',
         body: JSON.stringify({
           message,
-          threadId: threadId.value
+          threadId: threadId.value,
         }),
       },
     )
@@ -108,18 +106,17 @@ export const useOpenai = (props: UseOpenaiProps = {}) => {
       stream: stream.body,
       onChunk: (data) => {
         rawMarkdown += data
-        answer.content = md.render(rawMarkdown);
+        answer.content = md.render(rawMarkdown)
       },
       onReady: () => {
-        messages.push({ ...answer });
+        messages.push({ ...answer })
         rawMarkdown = ''
         answer.content = ''
       },
       onStart: () => {
         isLoading.value = false
-      }
+      },
     })
-
   }
 
   return { sendMessage, answer, isLoading, messages }
