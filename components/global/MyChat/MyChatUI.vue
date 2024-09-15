851d14ab02271b4fe942a8c3aa8f04424276594a
John Pino <iam@johnpino.me>
2024-09-15 10:14:21 -0500

    feat: adjust layout for better ux

--- components/global/MyChat/MyChatUI.vue
+++ components/global/MyChat/MyChatUI.vue
@@ -4,8 +4,28 @@
     <div class="max-w-[500px]">
       <div
         ref="scrollableAreaRef"
-        class="rich-text max-h-[300px] overflow-y-scroll rounded-t-md bg-slate-50 p-4 flex gap-4 flex-col border border-b-0"
+        class="rich-text h-80 overflow-y-scroll rounded-t-md bg-slate-50 p-4 flex gap-4 flex-col border border-b-0"
       >
+        <template v-if="queries?.length">
+          <div class="flex gap-4 flex-wrap">
+            <button
+              v-for="query in queries"
+              :key="query.value"
+              class="group rounded-sm text-xs font-light flex-shrink-0 flex items-center transition-all bg-slate-100 disabled:bg-slate-50 disabled:text-slate-300"
+              :disabled="query.wasSent || isLoading || !isMounted"
+              @click="sendQuery(query)"
+            >
+              <span class="px-3">{{ query.value }}</span>
+              <div class="p-3 border-l border-solid border-slate-100 bg-slate-200 group-disabled:bg-slate-50 group-hover:bg-rose-500 group-hover:text-white rounded-tr-sm rounder-br-sm transition-all">
+                <Icon
+                  name="fa6-regular:paper-plane"
+                  mode="svg"
+                />
+              </div>
+            </button>
+          </div>
+          <hr class="border-slate-200">
+        </template>
         <div
           v-for="message in messagesUI"
           :key="message.content?.toString()"
@@ -40,42 +60,19 @@
           :placeholder="!isMounted ? 'Loading...' : inputPlaceholder"
           required
         />
-        <div class="grid gap-2 lg:gap-4 lg:auto-cols-auto lg:grid-flow-col items-start">
-          <template v-if="queries?.length">
-            <div class="max-w-[500px] overflow-x-scroll pb-2">
-              <div class="flex gap-4">
-                <button
-                  v-for="query in queries"
-                  :key="query.value"
-                  class="group rounded-sm text-xs font-light flex-shrink-0 flex items-center transition-all bg-slate-100 disabled:bg-slate-50 disabled:text-slate-300"
-                  :disabled="query.wasSent || isLoading || !isMounted"
-                  @click="sendQuery(query)"
-                >
-                  <span class="px-3">{{ query.value }}</span>
-                  <div class="p-3 border-l border-solid border-slate-100 bg-slate-200 group-disabled:bg-slate-50 group-hover:bg-rose-500 group-hover:text-white rounded-tr-sm rounder-br-sm transition-all">
-                    <Icon
-                      name="fa6-regular:paper-plane"
-                      mode="svg"
-                    />
-                  </div>
-                </button>
-              </div>
-            </div>
-          </template>
-          <button
-            class="w-fit ml-auto bg-rose-500 rounded-sm text-white text-xs font-bold disabled:bg-rose-200 flex items-center flex-shrink-0 transition-all hover:bg-rose-700"
-            type="submit"
-            :disabled="isLoading || !isMounted"
-          >
-            <span class="px-3">{{ send }}</span>
-            <div class="p-3 border-l border-solid border-rose-300 rounded-tr-sm rounded-br-sm transition-all">
-              <Icon
-                name="fa6-regular:paper-plane"
-                mode="svg"
-              />
-            </div>
-          </button>
-        </div>
+        <button
+          class="w-fit ml-auto bg-rose-500 rounded-sm text-white text-xs font-bold disabled:bg-rose-200 flex items-center flex-shrink-0 transition-all hover:bg-rose-700"
+          type="submit"
+          :disabled="isLoading || !isMounted"
+        >
+          <span class="px-3">{{ send }}</span>
+          <div class="p-3 border-l border-solid border-rose-300 rounded-tr-sm rounded-br-sm transition-all">
+            <Icon
+              name="fa6-regular:paper-plane"
+              mode="svg"
+            />
+          </div>
+        </button>
       </form>
     </div>
   </div>
