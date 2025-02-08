4e15470fb215284d7ca3937b29a078d7e906b2e1
John Pino <iam@johnpino.me>
2025-02-08 12:18:58 -0500

    fix: adjust kv database provider

--- server/utils/queryKVDB.ts
+++ server/utils/queryKVDB.ts
@@ -1,18 +1,20 @@
-import { kv } from '@vercel/kv'
+import { Redis } from '@upstash/redis'
 
 type QueryKVDBProps = {
   key: string
 }
 
 export default async <U>({ key }: QueryKVDBProps) => {
-  const data = await kv.get<U>(key)
+  const redis = Redis.fromEnv()
+
+  const data = await redis.get<U>(key)
 
   const set = async (value: U, expiration: number = 60 * 60 * 24) => {
     if (data === null) {
-      return kv.set(key, value, { ex: expiration })
+      return redis.set(key, value, { ex: expiration })
     }
     else {
-      return kv.set(key, value, { keepTtl: true })
+      return redis.set(key, value, { keepTtl: true })
     }
   }
 
